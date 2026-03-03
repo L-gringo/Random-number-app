2b20a5cf2709b78c979d4c8de73a2ae131f70b83
mahmoulh <mahmoulh.aliou@yahoo.fr>
2026-03-03 11:19:32 +0100

    feat: add random button

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log ('hey');
+const randomNumberEL = document.querySelector("#random-number")
+randomNumberEL.addEventListener("click" , () => {
+   document.querySelector("#result").textContent = Math.random(); 
+})
